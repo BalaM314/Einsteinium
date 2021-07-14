1bb5d0be452349132b5f8367d1f4a961d5017797
BalaM314 <mail2balaadithya@gmail.com>
2021-07-14 10:26:21 +0530

    added the edit method

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 class Reactor {
   constructor(x, y, z){
     this.contents = [];
+    this.named = "Unnamed Reactor";
     let temp1 = [];
     let temp2 = [];
     for(let i = 0; i < z; i ++){
@@ -10,8 +11,26 @@ class Reactor {
       temp2.push(temp1);
     }
     for(let i = 0; i < y; i ++){
-      contents.push(temp2);
+      this.contents.push(temp2);
     }
+  }
+
+  edit(x, y, z, id){
+    if(isNan(id)){
+      console.error(`Invalid attempt to edit reactor 1 at position ${x},${y},${z} with bad id ${id}`);
+      return false;
+    }
+    try {
+      this.contents[z][x][y] = id;
+      return true;
+    } catch(err){
+      console.error(`Invalid attempt to edit reactor 1 at position ${x},${y},${z} with bad id ${id}`);
+      return false;
+    }
+  }
+
+  validate(){
+    return true;// TODO: make this actually validate
   }
 
   export(){

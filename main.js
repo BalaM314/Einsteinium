58e3aaa0665d7d35a1d5b5b5b5c29d818df9300f
BalaM314 <mail2balaadithya@gmail.com>
2021-07-14 13:57:45 +0530

    tweaked style, added function to actually load in a (blank)reactor

--- main.js
+++ main.js
@@ -1,3 +1,20 @@
+
+
+
+
+
+/**
+Block IDs:
+Air: 0
+Fuel Cell: 1
+Water Cooler: 2
+
+Beryllium Moderator: 18
+
+*/
+
+
+
 class Reactor {
   constructor(x, y, z){
     this.contents = [];
@@ -37,3 +54,28 @@ class Reactor {
     console.error("NYI!");
   }
 }
+
+
+var reactorLayers = document.getElementById("reactorlayers");
+
+
+function generateLayersToDOM(x, y, z){
+  x = constrain(x, 1, 17);
+  y = constrain(y, 1, 17);
+  z = constrain(z, 1, 17);
+  reactorLayers.attributes.dimensions = `${x},${y},${z}`;
+  let layerInnerHTML = `<div class="layerinner">`;
+  for(let i = 0; i < x*z; i ++){
+    layerInnerHTML += `<div class="cell"></div>`;
+  }
+  layerInnerHTML += "</div>";
+  for(let i = 0; i < z; i ++){
+    let tempElement = document.createElement("div");
+    tempElement.className = "layer";
+    tempElement.attributes.z = i;
+    tempElement.innerHTML = layerInnerHTML;
+    reactorLayers.appendChild(tempElement);
+  }
+}
+
+generateLayersToDOM(5, 5, 5);
